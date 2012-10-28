10fe4ef311debb3da618a2232e11bb32bcfb261a
Kenneth Lin <kenlin@berkeley.edu>
2012-10-28 14:55:56 -0700

    first commit

---
+++ auto/_region_.el
@@ -0,0 +1,17 @@
+(TeX-add-style-hook "_region_"
+ (lambda ()
+    (TeX-run-style-hooks
+     "multicol"
+     "setspace"
+     "geometry"
+     "a5paper"
+     "landscape"
+     "graphicx"
+     "amssymb"
+     "amsmath"
+     "latex2e"
+     "art10"
+     "article"
+     "twoside"
+     "draft")))
+
---
+++ auto/cheat.el
@@ -0,0 +1,17 @@
+(TeX-add-style-hook "cheat"
+ (lambda ()
+    (TeX-run-style-hooks
+     "multicol"
+     "setspace"
+     "geometry"
+     "a4paper"
+     "landscape"
+     "graphicx"
+     "amssymb"
+     "amsmath"
+     "latex2e"
+     "art10"
+     "article"
+     "twoside"
+     "draft")))
+
